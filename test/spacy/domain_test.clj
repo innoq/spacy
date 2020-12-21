856ac50b07bc7bf43a0b3edda64e85b843eb3e7c
Jan Stępień <jan.stepien@innoq.com>
2020-12-21 16:26:39 +0100

    Alias spacy.domain as domain in tests

--- test/spacy/domain_test.clj
+++ test/spacy/domain_test.clj
@@ -1,81 +1,80 @@
 (ns spacy.domain-test
-  (:require [spacy.domain :as sut]
+  (:require [spacy.domain :as domain]
             [clojure.test :as t :refer [deftest]]))
 
 (defn- random-uuid []
   (java.util.UUID/randomUUID))
 
 (defn test-event [& {:keys [next-up scheduled] :or {next-up (random-uuid)
-                                                         scheduled (random-uuid)}}]
-  {:spacy.domain/waiting-queue [#:spacy.domain{:sponsor "joy"
-                                                :session
-                                                #:spacy.domain{:title "Responsive and Accessible"
-                                                             :description "Responsible!"
-                                                             :id next-up}}]
-   :spacy.domain/rooms ["Berlin" "Monheim"]
-   :spacy.domain/times ["10:00 - 11:00" "11:00 - 12:00"]
-   :spacy.domain/schedule [#:spacy.domain{:sponsor "jans"
+                                                    scheduled (random-uuid)}}]
+  {::domain/waiting-queue [#:spacy.domain{:sponsor "joy"
                                           :session
-                                          #:spacy.domain{:title "Idris"
-                                                         :description "Ich liebe Typen"
-                                                         :id scheduled}
-                                          :room "Berlin"
-                                          :time "10:00 - 11:00"}]
-   :spacy.domain/facts []
-   :spacy.domain/slug "dezember-2020-strategie-event"})
+                                          #:spacy.domain{:title "Responsive and Accessible"
+                                                         :description "Responsible!"
+                                                         :id next-up}}]
+   ::domain/rooms ["Berlin" "Monheim"]
+   ::domain/times ["10:00 - 11:00" "11:00 - 12:00"]
+   ::domain/schedule [#:spacy.domain{:sponsor "jans"
+                                     :session
+                                     #:spacy.domain{:title "Idris"
+                                                    :description "Ich liebe Typen"
+                                                    :id scheduled}
+                                     :room "Berlin"
+                                     :time "10:00 - 11:00"}]
+   ::domain/facts []
+   ::domain/slug "dezember-2020-strategie-event"})
 
 (deftest test-can-schedule-session?
   (t/testing "Session with correct session id, room, and time can be scheduled"
     (t/is (some? (let [sid (random-uuid)]
-                   (sut/can-schedule-session?
+                   (domain/can-schedule-session?
                     (test-event :next-up sid) {:id sid :room "Berlin" :time "11:00 - 12:00"})))))
   (t/testing "Session to schedule must be the next in line"
     (t/is (not (let [sid (random-uuid)]
-                 (sut/can-schedule-session?
+                 (domain/can-schedule-session?
                   (test-event :next-up (random-uuid)) {:id sid :room "Berlin" :time "11:00 - 12:00"})))))
   (t/testing "Session must be for a valid room"
     (t/is (not (let [sid (random-uuid)]
-                 (sut/can-schedule-session?
+                 (domain/can-schedule-session?
                   (test-event :next-up sid) {:id sid :room "Non-existent room" :time "11:00 - 12:00"})))))
   (t/testing "Session must be for a valid time"
     (t/is (not (let [sid (random-uuid)]
-                 (sut/can-schedule-session?
+                 (domain/can-schedule-session?
                   (test-event :next-up sid) {:id sid :room "Berlin" :time "Non-existent time"})))))
   (t/testing "Session cannot be placed in a slot which is already taken"
     (t/is (not (let [sid (random-uuid)]
-                 (sut/can-schedule-session?
+                 (domain/can-schedule-session?
                   (test-event :next-up sid) {:id sid :room "Berlin" :time "10:00 - 11:00"}))))))
-
 
 (deftest test-schedule-session
   (t/testing "Test scheduling of event"
     (let [sid (random-uuid)
           _user nil
           outcome (-> (test-event :next-up sid)
-                      (sut/schedule-session _user {:id sid :room "Berlin" :time "11:00 - 12:00"}))
-          {:spacy.domain/keys [event facts]} outcome]
-      (t/is (empty? (:spacy.domain/waiting-queue event)))
-      (t/is (some   (fn [s] (= (get-in s [:spacy.domain/session :spacy.domain/id]) sid))
-                    (:spacy.domain/schedule event)))
-      (t/is (some   (fn [f] (= (:spacy.domain/fact f) :spacy.domain/session-scheduled))
+                      (domain/schedule-session _user {:id sid :room "Berlin" :time "11:00 - 12:00"}))
+          {::domain/keys [event facts]} outcome]
+      (t/is (empty? (::domain/waiting-queue event)))
+      (t/is (some   (fn [s] (= (get-in s [::domain/session ::domain/id]) sid))
+                    (::domain/schedule event)))
+      (t/is (some   (fn [f] (= (::domain/fact f) ::domain/session-scheduled))
                     facts)))))
 
 (deftest test-waiting-queue
   (t/testing "Interleaving suggest and schedule actions"
     (let [sid (random-uuid)
           event (-> (test-event :next-up sid)
-                    (sut/suggest-session "sponsor" {:title "one", :description ""})
-                    :spacy.domain/event
-                    (sut/schedule-session "sponsor" {:id sid :room "Berlin" :time "11:00 - 12:00"})
-                    :spacy.domain/event
-                    (sut/suggest-session "sponsor" {:title "two", :description ""})
-                    :spacy.domain/event
-                    (sut/suggest-session "sponsor" {:title "three", :description ""})
-                    :spacy.domain/event)
+                    (domain/suggest-session "sponsor" {:title "one", :description ""})
+                    ::domain/event
+                    (domain/schedule-session "sponsor" {:id sid :room "Berlin" :time "11:00 - 12:00"})
+                    ::domain/event
+                    (domain/suggest-session "sponsor" {:title "two", :description ""})
+                    ::domain/event
+                    (domain/suggest-session "sponsor" {:title "three", :description ""})
+                    ::domain/event)
           waiting-queued-titles (->> event
-                                     :spacy.domain/waiting-queue
-                                     (map (comp :spacy.domain/title
-                                                :spacy.domain/session)))]
+                                     ::domain/waiting-queue
+                                     (map (comp ::domain/title
+                                                ::domain/session)))]
       (t/is (= ["one" "two" "three"]
                waiting-queued-titles)))))
 
@@ -84,36 +83,36 @@
   (t/testing "Session you're not sponsoring"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/delete-session "jans" {:id sid}))]
-      (t/is (:spacy.domain/error outcome))))
+                      (domain/delete-session "jans" {:id sid}))]
+      (t/is (::domain/error outcome))))
 
   (t/testing "Non-existent session"
     (let [sid (random-uuid)
           made-up-id (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/delete-session "joy" {:id made-up-id}))]
-      (t/is (:spacy.domain/error outcome))))
+                      (domain/delete-session "joy" {:id made-up-id}))]
+      (t/is (::domain/error outcome))))
 
   (t/testing "Suggested session"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/delete-session "joy" {:id sid}))
-          {:spacy.domain/keys [event facts]} outcome]
-      (t/is (empty? (:spacy.domain/waiting-queue event)))
-      (t/is (some (fn [f] (= (:spacy.domain/fact f) :spacy.domain/session-deleted))
+                      (domain/delete-session "joy" {:id sid}))
+          {::domain/keys [event facts]} outcome]
+      (t/is (empty? (::domain/waiting-queue event)))
+      (t/is (some (fn [f] (= (::domain/fact f) ::domain/session-deleted))
                   facts))))
 
   (t/testing "Scheduled session"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
-                      :spacy.domain/event
-                      (sut/delete-session "joy" {:id sid}))
-          {:spacy.domain/keys [event facts]} outcome]
-      (t/is (empty? (:spacy.domain/waiting-queue event)))
-      (t/is (empty? (->> (:spacy.domain/schedule event)
-                         (filter (fn [s] (= (get-in s [:spacy.domain/session :spacy.domain/id]) sid))))))
-      (t/is (some (fn [f] (= (:spacy.domain/fact f) :spacy.domain/session-deleted))
+                      (domain/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
+                      ::domain/event
+                      (domain/delete-session "joy" {:id sid}))
+          {::domain/keys [event facts]} outcome]
+      (t/is (empty? (::domain/waiting-queue event)))
+      (t/is (empty? (->> (::domain/schedule event)
+                         (filter (fn [s] (= (get-in s [::domain/session ::domain/id]) sid))))))
+      (t/is (some (fn [f] (= (::domain/fact f) ::domain/session-deleted))
                   facts)))))
 
 (deftest test-move-session
@@ -121,36 +120,36 @@
   (t/testing "Session in the waiting queue"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/move-session "joy" {:id sid :room "Monheim" :time "11:00 - 12:00"}))]
-      (t/is (:spacy.domain/error outcome))))
+                      (domain/move-session "joy" {:id sid :room "Monheim" :time "11:00 - 12:00"}))]
+      (t/is (::domain/error outcome))))
 
   (t/testing "Session you're not sponsoring"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
-                      :spacy.domain/event
-                      (sut/move-session "jans" {:id sid :room "Monheim" :time "11:00 - 12:00"}))]
-      (t/is (:spacy.domain/error outcome))))
+                      (domain/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
+                      ::domain/event
+                      (domain/move-session "jans" {:id sid :room "Monheim" :time "11:00 - 12:00"}))]
+      (t/is (::domain/error outcome))))
 
   (t/testing "Scheduled session to slot that's not open"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
-                      :spacy.domain/event
-                      (sut/move-session "joy" {:id sid :room "Berlin" :time "10:00 - 11:00"}))]
-      (t/is (:spacy.domain/error outcome))))
+                      (domain/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
+                      ::domain/event
+                      (domain/move-session "joy" {:id sid :room "Berlin" :time "10:00 - 11:00"}))]
+      (t/is (::domain/error outcome))))
 
   (t/testing "Scheduled session to an open slot"
     (let [sid (random-uuid)
           outcome (-> (test-event :next-up sid)
-                      (sut/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
-                      :spacy.domain/event
-                      (sut/move-session "joy" {:id sid :room "Monheim" :time "11:00 - 12:00"}))
-          {:spacy.domain/keys [event facts]} outcome
-          moved-session (->> (:spacy.domain/schedule event)
-                             (filter (fn [s] (= (get-in s [:spacy.domain/session :spacy.domain/id])
+                      (domain/schedule-session "joy" {:id sid :room "Berlin" :time "11:00 - 12:00"})
+                      ::domain/event
+                      (domain/move-session "joy" {:id sid :room "Monheim" :time "11:00 - 12:00"}))
+          {::domain/keys [event facts]} outcome
+          moved-session (->> (::domain/schedule event)
+                             (filter (fn [s] (= (get-in s [::domain/session ::domain/id])
                                                 sid)))
                              first)]
-      (t/is (= "Monheim" (:spacy.domain/room moved-session)))
-      (t/is (some (fn [f] (= (:spacy.domain/fact f) :spacy.domain/session-moved))
+      (t/is (= "Monheim" (::domain/room moved-session)))
+      (t/is (some (fn [f] (= (::domain/fact f) ::domain/session-moved))
                   facts)))))

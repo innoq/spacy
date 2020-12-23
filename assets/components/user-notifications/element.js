0c70134cfd902213dc7be87fc60fd7a6b018b914
Joy Heron <joy.heron@innoq.com>
2020-12-23 20:41:25 +0100

    Refactoring statuses to use role=region and aria-live=polite
    This refactors the user notification system (tested on screenreaders).
    The hope is to find a system where user updates _that are actually useful_
    are read out at the right time (only when something changed and we want
    to inform the user about it).
    
    I didn't really like the old usage of role=log, because the user only is
    really interested in a status update once, and doesn't want to go back
    and read old entries (this is an assumption. could be wrong about that).
    
    So I've exchanged the role=log section for areas which are role=region
    as well as aria-live=polite. I have (tried) to test this with a screenreader,
    and it seems to work, but will need more testing in the future.

--- assets/components/user-notifications/element.js
+++ assets/components/user-notifications/element.js
@@ -2,13 +2,11 @@ import { fillTemplate } from "../util/template";
 
 export class UserNotifications extends HTMLElement {
   connectedCallback() {
-    if (!this.entries) {
+    if (!this.status || !this.listenFor) {
       return;
     }
-    this.hidden = false;
 
-    document.body.addEventListener("spacy.domain/session-suggested", this.addNotification.bind(this));
-    document.body.addEventListener("spacy.domain/session-scheduled", this.addNotification.bind(this));
+    this.listenFor.forEach(fact => document.body.addEventListener(fact, this.addNotification.bind(this)));
   }
 
   addNotification(ev) {
@@ -16,43 +14,45 @@ export class UserNotifications extends HTMLElement {
     const session = ev.detail["spacy.domain/session"];
     const notification = this.newNotification(ev.type);
 
-    if (!session || !this.notifyFor(ev.type, sponsor) || !notification) {
+    this.status.innerHTML = '';
+
+    if (!session || !this.notifyFor(sponsor) || !notification) {
       return; // Add no notifications for things we don't understand
     }
 
     fillTemplate(notification, "title", session["spacy.domain/title"]);
     fillTemplate(notification, "sponsor", sponsor);
-    fillTemplate(notification, "at", new Date()); // TODO: use Crux timestamp, but deal with timezones
     fillTemplate(notification, "room", ev.detail["spacy.domain/room"]);
     fillTemplate(notification, "time", ev.detail["spacy.domain/time"]);
 
-    this.entries.appendChild(notification);
+    this.status.appendChild(notification);
   }
 
-  get entries() {
-    return this.querySelector("ul");
+  get status() {
+    return this.querySelector("small");
   }
 
   get currentUser() {
     return this.getAttribute("current-user");
   }
 
-  notifyFor(fact, sponsor) {
-    switch (fact) {
-      case "spacy.domain/session-scheduled":
-        return true;
-      default:
-        return sponsor === this.currentUser;
+  get listenFor() {
+    let nodes = this.querySelectorAll("[data-fact]");
+    return [...nodes].map(el => el.getAttribute("data-fact"));
+  }
+
+  notifyFor(sponsor) {
+    if (this.hasAttribute("notify-all")) {
+      return true;
     }
+    return sponsor === this.currentUser;
   }
 
   newNotification(fact) {
-    const template = this.querySelector(`template[data-template="${fact}"]`);
+    const template = this.querySelector(`template[data-fact="${fact}"]`);
     if (!template) {
       return;
     }
-    return template.content
-      .querySelector("*") // Find first true HTML node which will be our session markup
-      .cloneNode(true);
+    return template.content.cloneNode(true);
   }
 }
